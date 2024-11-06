82605a619eb87e5d09f52fe695882c68afed085e
takahiro-maekawa <koma4024@gmail.com>
2024-11-06 21:43:49 +0900

    コードリファクタ
    余計なものが混じっていたので削除

--- aws/env/dev/infra/main.tf
+++ aws/env/dev/infra/main.tf
@@ -6,19 +6,3 @@ module "network" {
   route_table_id = var.route_table_id
 }
 
-
-/**
-module "lambda_image_viewer" {
-  source               = "../../../modules/lambda/imageViewer"
-  lambda_function_name = "lambda_image_viewer"
-  ecr_repository_name  = "img_viewer2_nextjs_action"
-  application_port     = "3030"
-}
-
-module "lambda_my_good_stuff" {
-  source               = "../../../modules/lambda/myGoodStuff"
-  lambda_function_name = "my_good_stuff"
-  ecr_repository_name  = "my_good_stuff_action"
-  application_port     = "3030"
-}
-**/
